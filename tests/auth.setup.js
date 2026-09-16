67711eab0b2eeb801a81eb023818972390344c9a
Nikolay Kolev <nikitospeeder@abv.bg>
2026-09-16 16:34:37 +0300

    add another checks

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -7,19 +7,19 @@ const authFile = 'playwright/.auth/user.json';
 setup("authenticate user", async({page})=> {
     const loginPage = new LoginPage(page);
     await page.goto("/login")
-     const loginResponsePromise = page.waitForResponse(
-        response =>
-            response.url().includes('/Account/v1/Login')
-    );
+    page.on('response', response => {
+        if (response.url().includes('/Account/')) {
+            console.log(
+                'ACCOUNT RESPONSE:',
+                response.status(),
+                response.url()
+            );
+        }
+    });
     await loginPage.login(
         process.env.E2E_USERNAME,
         process.env.E2E_PASSWORD
     )
-     const loginResponse = await loginResponsePromise;
-
-    console.log('Login API status:', loginResponse.status());
-    console.log('Login API response:', await loginResponse.text());
-
     await expect(page).toHaveURL(/profile/)
 
     await page.context().storageState({
