e0cb643943b496b8d18c5c40f3135be7088a7d43
Nikolay Kolev <nikitospeeder@abv.bg>
2026-09-16 16:45:49 +0300

    add new checks

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -7,12 +7,17 @@ const authFile = 'playwright/.auth/user.json';
 setup("authenticate user", async({page})=> {
     const loginPage = new LoginPage(page);
     await page.goto("/login")
-    page.on('response', response => {
-        if (response.url().includes('/Account/')) {
+    page.on('response', async response => {
+        if (response.url().includes('/Account/v1/GenerateToken')) {
             console.log(
                 'ACCOUNT RESPONSE:',
                 response.status(),
                 response.url()
+            );
+
+            console.log(
+                'ACCOUNT BODY:',
+                await response.text()
             );
         }
     });
